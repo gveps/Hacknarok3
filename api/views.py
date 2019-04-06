dc464bee4e88b1289c38fe04e774dad1dcb340f9
PiotrKedra <piotr.k9750@gmail.com>
2019-04-06 21:22:30 +0200

    front

--- api/views.py
+++ api/views.py
@@ -14,5 +14,34 @@ def upload(request):
         print(upload_file.size)
         fs = FileSystemStorage()
         fs.save(upload_file.name, upload_file.size)
-        print('dupa')
     return render(request, 'api/upload.html')
+
+
+def task(request):
+    return render(request, 'api/task.html')
+
+
+def daily(request):
+    return render(request, 'api/daily.html')
+
+
+def account(request):
+    return render(request, 'api/account.html')
+
+
+def competition(request):
+    return render(request, 'api/competition.html')
+
+
+def home(request):
+    return render(request, 'api/task.html')
+
+
+def new_task(request):
+    if request.method == 'POST':
+        print(request.POST.get('task_name'))
+        print(request.POST.get('task_description'))
+        print(request.POST.get('task_deadline'))
+        return render(request, 'api/task.html')
+    return render(request, 'api/new_task.html')
+
