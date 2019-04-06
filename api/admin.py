cb159947f19fb2df8a63f474f374ba660b124248
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-04-06 21:54:01 +0200

    Created database access

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from api.models import Task, Tag, TagTask, TaskUser
+from api.models import Task, Tag, TagTask, TaskUser, MyUser, Challange, TaskChallange
 
 admin.site.register(Task)
 admin.site.register(Tag)
 admin.site.register(TagTask)
 admin.site.register(TaskUser)
+admin.site.register(MyUser)
+admin.site.register(Challange)
+admin.site.register(TaskChallange)
--- api/models.py
+++ api/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
+    total_times = models.IntegerField()
+    startDate = models.DateField()
+    endDate = models.DateField()
+    type = models.IntegerField(None)
+    status = models.BooleanField(None)
 
 
 class Tag(models.Model):
@@ -16,10 +21,28 @@ class TagTask(models.Model):
     id_task = models.ForeignKey(Task, on_delete=models.CASCADE)
 
 
+class MyUser(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    photo = models.CharField(max_length=30)
+
+
 class TaskUser(models.Model):
-    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     id_task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    is_verified = models.BooleanField()
-    deadline = models.DateField()
+    counter = models.IntegerField()
+    last_time = models.DateField()
 
 
+class Challange(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(None)
+    bet_value = models.DecimalField(max_digits=5, decimal_places=2)
+    status = models.BooleanField(default=True)
+
+
+class TaskChallange(models.Model):
+    id_challange = models.ForeignKey(Challange, on_delete=models.CASCADE)
+    id_task = models.ForeignKey(Task, on_delete=models.CASCADE)
